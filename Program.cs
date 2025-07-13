b1afeb6d0b6c5187359562d4b02d948491e5993e
Erick Thamara <erickthamara9@gmail.com>
2025-07-13 21:10:49 +0300

    Remove trailing slash from CORS allowed origin
    Updated the CORS policy to use 'https://au-whitelisting-demo.erickthamara.com' without a trailing slash. This ensures proper origin matching for cross-origin requests.

--- Program.cs
+++ Program.cs
@@ -93,7 +93,7 @@ var myAllowedOrigins = "_WhitelistingApp";
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: myAllowedOrigins,
-       policy => policy.WithOrigins("https://au-whitelisting-demo.erickthamara.com/")
+       policy => policy.WithOrigins("https://au-whitelisting-demo.erickthamara.com")
        .WithMethods("PUT", "DELETE", "GET", "POST")
        .AllowAnyHeader()
        .AllowCredentials());
